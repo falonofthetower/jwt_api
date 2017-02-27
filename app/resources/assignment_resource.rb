8976f869135285a7eb0c02b2e1945d29483d3017
Peter Karth <falonofthetower@gmail.com>
2017-02-27 14:43:01 -0500

    task assigner set to current user

--- app/resources/assignment_resource.rb
+++ app/resources/assignment_resource.rb
@@ -3,4 +3,8 @@ class AssignmentResource < JSONAPI::Resource
   belongs_to :assigner
   belongs_to :assignee
   belongs_to :todo
+
+  before_save do
+    @model.assigner_id = context[:current_user].id if @model.new_record?
+  end
 end
