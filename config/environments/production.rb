f8815bc3ac7ab5d49fd81b781d179848a0b432d8
Peter Karth <falonofthetower@gmail.com>
2017-04-02 11:13:34 -0400

    fixing redis in prod setup

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,4 +75,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.action_cable.url = "ws://task-master-api-staging.herokuapp.com/websocket"
+  config.action_cable.allowed_request_origins = [/http:\/\/*/, /https:\/\/*/]
 end
